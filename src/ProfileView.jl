821b93a26111f452e4a752d55433830cb8937741
timholy <tim.holy@gmail.com>
2014-04-02 23:51:26 -0500

    Mostly implement interactive display in IJulia
    Mostly fixes #2
    
    Remaining bug: hovering over rectangles doesn't print the info on the "Function:" status line

--- src/ProfileView.jl
+++ src/ProfileView.jl
@@ -1,13 +1,24 @@
 module ProfileView
 
-using Tk, Color, Base.Graphics
-import Cairo
+using Color, Base.Graphics
 
-import Base: contains, isequal, show
+useTk = !isdefined(Main, :IJulia)
+if useTk
+    using Tk
+    import Cairo
+    include(joinpath(Pkg.dir(), "ImageView", "src", "rubberband.jl")) # for zoom
+    type ZoomCanvas
+        bb::BoundingBox  # in user-coordinates
+        c::Canvas
+    end
+else
+    include("svgwriter.jl")
+end
+
+import Base: contains, isequal, show, mimewritable, writemime
 
 include("tree.jl")
 include("pvtree.jl")
-include(joinpath(Pkg.dir(), "ImageView", "src", "rubberband.jl")) # for zoom
 
 using .Tree
 using .PVTree
@@ -18,17 +29,19 @@ immutable TagData
 end
 const TAGNONE = TagData(uint(0), -1)
 
+type ProfileData
+    img
+    lidict
+    imgtags
+    fontsize
+end
+
 const bkg = color("black")
 const fontcolor = color("white")
 const gccolor = color("red")
 const colors = distinguishable_colors(13, [bkg,fontcolor,gccolor])[4:end]
 
-type ZoomCanvas
-    bb::BoundingBox  # in user-coordinates
-    c::Canvas
-end
-
-function view(data = Profile.fetch(); C = false, colorgc = true, fontsize = 12, combine = true)
+function prepare(data; C = false, colorgc = true, combine = true)
     bt, counts = Profile.tree_aggregate(data)
     if isempty(counts)
         Profile.warning_empty()
@@ -36,6 +49,9 @@ function view(data = Profile.fetch(); C = false, colorgc = true, fontsize = 12, 
     end
     len = Int[length(x) for x in bt]
     keep = len .> 0
+    if length(data) == Profile.maxlen_data()
+        keep[end] = false
+    end
     bt = bt[keep]
     counts = counts[keep]
     # Do code address lookups on all unique instruction pointers
@@ -86,101 +102,198 @@ function view(data = Profile.fetch(); C = false, colorgc = true, fontsize = 12, 
     buildtags!(rowtags, root, 1)
     imgtags = hcat(rowtags...)
     img = buildimg(imgtags, colors, bkg, gccolor, colorgc, combine, lidict)
-    img24 = [convert(Uint32, convert(RGB24, img[i,j])) for i = 1:size(img,1), j = size(img,2):-1:1]'
-    surf = Cairo.CairoRGBSurface(img24)
-    imw = size(img24,2)
-    imh = size(img24,1)
-    # Display in a window
-    win = Toplevel("Profile", 300, 300)
-    f = Frame(win)
-    pack(f, expand = true, fill = "both")
-    c = Canvas(f)
-    pack(c, expand = true, fill = "both")
-    czoom = ZoomCanvas(BoundingBox(0, imw, 0, imh), c)
-    c.mouse.button1press = (c, x, y) -> rubberband_start(c, x, y, (c, bb) -> zoom_bb(czoom, bb))
-    bind(c, "<Double-Button-1>", (path,x,y)->zoom_reset(czoom))
-    lasttextbb = BoundingBox(1,0,1,0)
-    imgbb = BoundingBox(0, imw, 0, imh)
-    function zoom_bb(czoom::ZoomCanvas, bb::BoundingBox)
-        czoom.bb = bb & imgbb
-        redraw(czoom.c)
-        reveal(czoom.c)
-        Tk.update()
-    end
-    function zoom_reset(czoom::ZoomCanvas)
-        czoom.bb = imgbb
-        redraw(czoom.c)
-        reveal(czoom.c)
-        Tk.update()
-    end
-    function redraw(c)
-        ctx = getgc(c)
-        w = width(c)
-        h = height(c)
-        cbb = czoom.bb
-        winbb = BoundingBox(0, w, 0, h)
-        set_coords(ctx, winbb, cbb)
-        rectangle(ctx, cbb)
-        set_source(ctx, surf)
-        p = Cairo.get_source(ctx)
-        Cairo.pattern_set_filter(p, Cairo.FILTER_NEAREST)
-        fill(ctx)
-    end
-    # From a given position, find the underlying tag
-    function gettag(xu, yu)
-        x = iceil(xu)
-        y = iceil(yu)
-        Y = size(imgtags, 2)
-        x = max(1, min(x, size(imgtags, 1)))
-        y = max(1, min(y, Y))
-        imgtags[x,Y-y+1]
-    end
-    c.resize = function (_)
-        redraw(c)
-        reveal(c)
-        Tk.update()
-    end
-    # Hover over a block and see the source line
-    c.mouse.motion = function (c, xd, yd)
-        # Repair image from ovewritten text
-        ctx = getgc(c)
-        w = width(c)
-        if width(lasttextbb) > 0
+    img, lidict, imgtags
+end
+
+if useTk
+    function view(data = Profile.fetch(); C = false, colorgc = true, fontsize = 12, combine = true)
+        img, lidict, imgtags = prepare(data, C=C, colorgc=colorgc, combine=combine)
+        img24 = [convert(Uint32, convert(RGB24, img[i,j])) for i = 1:size(img,1), j = size(img,2):-1:1]'
+        surf = Cairo.CairoRGBSurface(img24)
+        imw = size(img24,2)
+        imh = size(img24,1)
+        # Display in a window
+        win = Toplevel("Profile", 300, 300)
+        f = Frame(win)
+        pack(f, expand = true, fill = "both")
+        c = Canvas(f)
+        pack(c, expand = true, fill = "both")
+        czoom = ZoomCanvas(BoundingBox(0, imw, 0, imh), c)
+        c.mouse.button1press = (c, x, y) -> rubberband_start(c, x, y, (c, bb) -> zoom_bb(czoom, bb))
+        bind(c, "<Double-Button-1>", (path,x,y)->zoom_reset(czoom))
+        lasttextbb = BoundingBox(1,0,1,0)
+        imgbb = BoundingBox(0, imw, 0, imh)
+        function zoom_bb(czoom::ZoomCanvas, bb::BoundingBox)
+            czoom.bb = bb & imgbb
+            redraw(czoom.c)
+            reveal(czoom.c)
+            Tk.update()
+        end
+        function zoom_reset(czoom::ZoomCanvas)
+            czoom.bb = imgbb
+            redraw(czoom.c)
+            reveal(czoom.c)
+            Tk.update()
+        end
+        function redraw(c)
+            ctx = getgc(c)
+            w = width(c)
             h = height(c)
+            cbb = czoom.bb
             winbb = BoundingBox(0, w, 0, h)
-            set_coords(ctx, winbb, czoom.bb)
-            rectangle(ctx, lasttextbb)
+            set_coords(ctx, winbb, cbb)
+            rectangle(ctx, cbb)
             set_source(ctx, surf)
             p = Cairo.get_source(ctx)
             Cairo.pattern_set_filter(p, Cairo.FILTER_NEAREST)
             fill(ctx)
         end
-        # Write the info
-        xu, yu = device_to_user(ctx, xd, yd)
-        tag = gettag(xu, yu)
-        if tag != TAGNONE
-            li = lidict[tag.ip]
-            str = string(basename(li.file), ", ", li.func, ": line ", li.line)
-            set_source(ctx, fontcolor)
-            Cairo.set_font_face(ctx, "sans-serif $(fontsize)px")
-            lasttextbb = Cairo.text(ctx, xu, yu, str, halign = xd < w/3 ? "left" : xd < 2w/3 ? "center" : "right")
+        # From a given position, find the underlying tag
+        function gettag(xu, yu)
+            x = iceil(xu)
+            y = iceil(yu)
+            Y = size(imgtags, 2)
+            x = max(1, min(x, size(imgtags, 1)))
+            y = max(1, min(y, Y))
+            imgtags[x,Y-y+1]
         end
-        reveal(c)
-        Tk.update()
+        c.resize = function (_)
+            redraw(c)
+            reveal(c)
+            Tk.update()
+        end
+        # Hover over a block and see the source line
+        c.mouse.motion = function (c, xd, yd)
+            # Repair image from ovewritten text
+            ctx = getgc(c)
+            w = width(c)
+            if width(lasttextbb) > 0
+                h = height(c)
+                winbb = BoundingBox(0, w, 0, h)
+                set_coords(ctx, winbb, czoom.bb)
+                rectangle(ctx, lasttextbb)
+                set_source(ctx, surf)
+                p = Cairo.get_source(ctx)
+                Cairo.pattern_set_filter(p, Cairo.FILTER_NEAREST)
+                fill(ctx)
+            end
+            # Write the info
+            xu, yu = device_to_user(ctx, xd, yd)
+            tag = gettag(xu, yu)
+            if tag != TAGNONE
+                li = lidict[tag.ip]
+                str = string(basename(li.file), ", ", li.func, ": line ", li.line)
+                set_source(ctx, fontcolor)
+                Cairo.set_font_face(ctx, "sans-serif $(fontsize)px")
+                lasttextbb = Cairo.text(ctx, xu, yu, str, halign = xd < w/3 ? "left" : xd < 2w/3 ? "center" : "right")
+            end
+            reveal(c)
+            Tk.update()
+        end
+        # Right-click prints the full path, function, and line to the console
+        c.mouse.button3press = function (c, xd, yd)
+            ctx = getgc(c)
+            xu, yu = device_to_user(ctx, xd, yd)
+            tag = gettag(xu, yu)
+            if tag != TAGNONE
+                li = lidict[tag.ip]
+                println(li.file, ", ", li.func, ": line ", li.line)
+            end
+        end
+        set_size(win, 300, 300)
+        c.resize(c)
+        nothing
     end
-    # Right-click prints the full path, function, and line to the console
-    c.mouse.button3press = function (c, xd, yd)
-        ctx = getgc(c)
-        xu, yu = device_to_user(ctx, xd, yd)
-        tag = gettag(xu, yu)
-        if tag != TAGNONE
-            li = lidict[tag.ip]
-            println(li.file, ", ", li.func, ": line ", li.line)
+else
+    function view(data = Profile.fetch(); C = false, colorgc = true, fontsize = 12, combine = true)
+        img, lidict, imgtags = prepare(data, C=C, colorgc=colorgc, combine=combine)
+        ProfileData(img, lidict, imgtags, fontsize)
+    end
+end
+
+mimewritable(::MIME"image/svg+xml", pd::ProfileData) = true
+
+function writemime(f::IO, ::MIME"image/svg+xml", pd::ProfileData)
+    img = pd.img
+    lidict = pd.lidict
+    imgtags = pd.imgtags
+    fontsize = pd.fontsize
+
+    ncols, nrows = size(img)
+    leftmargin = rightmargin = 10
+    width = 1200
+    topmargin = 30
+    botmargin = 40
+    rowheight = 15
+    height = ceil(rowheight*nrows + botmargin + topmargin)
+    xstep = (width - (leftmargin + rightmargin)) / ncols
+    ystep = (height - (topmargin + botmargin)) / nrows
+    avgcharwidth = 6  # for Verdana 12 pt font
+    function eschtml(str)
+        s = replace(str, '<', "&lt;")
+        s = replace(s, '>', "&gt;")
+        s = replace(s, '&', "&amp;")
+        s
+    end
+    function printrec(f, samples, xstart, xend, y, tag, rgb)
+        width = xend - xstart
+        li = lidict[tag.ip]
+        info = "$(li.func) in $(li.file):$(li.line)"
+        shortinfo = info
+        if avgcharwidth*3 > width
+            shortinfo = ""
+        elseif length(shortinfo) * avgcharwidth > width
+            nchars = int(width/avgcharwidth)-2
+            shortinfo = eschtml(info[1:nchars] * "..")
+        end
+        info = eschtml(info)
+        red = iround(255*rgb.r)
+        green = iround(255*rgb.g)
+        blue = iround(255*rgb.b)
+        print(f, """<rect x="$xstart" y="$y" width="$width" height="$ystep" fill="rgb($red,$green,$blue)" rx="2" ry="2" onmouseover="s('$info')" onmouseout="c()"/>""")
+        if shortinfo != ""
+            println(f, """\n<text text-anchor="" x="$(xstart+4)" y="$(y+11.5)" font-size="12" font-family="Verdana" fill="rgb(0,0,0)" onmouseover="s('$info')" onmouseout="c()">\n$shortinfo\n</text>""")
         end
     end
-    set_size(win, 300, 300)
-    c.resize(c)
-    nothing
+
+    svgheader(f, width=width, height=height)
+    # rectangles are on a grid and split across multiple columns (must span similar adjacent ones together)
+    for r in 1:nrows
+        # top of rectangle:
+        y = height - r*ystep - botmargin
+        # local vars:
+        prevtag = TAGNONE
+        xstart = xend = 0.0
+        for c in 1:ncols
+            tag = imgtags[c,r]
+            if prevtag == TAGNONE && prevtag != tag
+                # Very first in span
+                xstart = (c-1) * xstep + leftmargin
+            elseif tag != prevtag && tag != TAGNONE && prevtag != TAGNONE
+                # End of old span and start of new one
+                xend = (c-1) * xstep + leftmargin
+                samples = int(round((xend - xstart)/xstep))
+                printrec(f, samples, xstart, xend, y, prevtag, img[c-1,r])
+                xstart = xend
+            elseif tag == TAGNONE && tag != prevtag
+                # at end of span and start of nothing
+                xend = (c-1) * xstep + leftmargin
+                samples = int(round((xend - xstart)/xstep))
+                printrec(f, samples, xstart, xend, y, prevtag, img[c-1,r])
+                xstart = 0.0
+            elseif c == ncols && tag != TAGNONE
+                # end of span at last element of row
+                xend = (c-1) * xstep + leftmargin
+                samples = int(round((xend - xstart)/xstep))
+                printrec(f, samples, xstart, xend, y, tag, img[c,r])
+                xstart = 0.0
+            else
+                # in middle of span
+            end
+            prevtag = tag
+        end
+    end
+    println(f, "\n</svg>")
 end
 
 function buildtags!(rowtags, parent, level)
